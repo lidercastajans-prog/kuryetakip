9b2845f8d07e455c2784bf747e48b10799cf69cc
belucaunal <beluckangoz@gmail.com>
2026-06-02 00:56:25 +0300

    fix(auth): web Google OAuth fix

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,15 +1,18 @@
 import 'react-native-url-polyfill/auto'
 import { createClient } from '@supabase/supabase-js'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { Platform } from 'react-native'
 
 const supabaseUrl = 'https://nfdzipkrkphvkhybjuvw.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5mZHppcGtya3BodmtoeWJqdXZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzMzMxMzMsImV4cCI6MjA5NTkwOTEzM30.ISng_jfheSK0BV2DcSBmYJIqcb2EuJ6Q4ODyxKmGfWM'
 
+const isWeb = Platform.OS === 'web'
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    storage: AsyncStorage,
+    storage: isWeb ? undefined : AsyncStorage,
     autoRefreshToken: true,
     persistSession: true,
-    detectSessionInUrl: false,
+    detectSessionInUrl: isWeb,
   },
 })
