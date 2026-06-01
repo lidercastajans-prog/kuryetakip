d1d7e62d96cddb1f31a5cffbc3b6705720093f63
belucaunal <beluckangoz@gmail.com>
2026-06-02 00:31:00 +0300

    feat: Yeni Supabase projesine geçiş

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,8 +2,8 @@ import 'react-native-url-polyfill/auto'
 import { createClient } from '@supabase/supabase-js'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
-const supabaseUrl = 'https://xwkhnxubxoqclwizhojf.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3a2hueHVieG9xY2x3aXpob2pmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1MzI3MzIsImV4cCI6MjA5MDEwODczMn0.6Z2Q0mI_GpHHRylWiotpmzODtBg7JEPJMeUp80vUTJ8'
+const supabaseUrl = 'https://nfdzipkrkphvkhybjuvw.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5mZHppcGtya3BodmtoeWJqdXZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzMzMxMzMsImV4cCI6MjA5NTkwOTEzM30.ISng_jfheSK0BV2DcSBmYJIqcb2EuJ6Q4ODyxKmGfWM'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
